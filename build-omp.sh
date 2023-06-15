315ddaa2c8674f745dd3237fd05dea7b7984d1c5
R. Tohid <tohid.rod@gmail.com>
2023-06-15 10:54:32 -0500

    Add license.

--- build-omp.sh
+++ build-omp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) 2023 R. Tohid (@rtohid)
+#
+# SPDX-License-Identifier: BSL-1.0
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 module load gcc/12.3.0 boost
 
 set -e
